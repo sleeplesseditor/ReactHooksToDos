d34496ecb70ad814b85be2a37d470f63582bf885
Josh Bryson <josh@bryson.me.uk>
2019-05-22 13:31:37 +0100

    Initial component and Hooks setup

---
+++ src/context.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodosContext = React.createContext({
+    todos: [
+        { id: 1, text: "Kick Ass", complete: false },
+        { id: 2, text: "Chew Bubblegum", complete: true },
+        { id: 3, text: "Acquire More Bubblegum", complete: false },
+    ]
+})
+
+export default TodosContext;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import TodosContext from './context';
+import todosReducer from './reducer';
+import TodosList from './components/TodoList';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    const initialState = useContext(TodosContext);
+    const [state, dispatch] = useReducer(todosReducer, initialState);
+
+    return (
+        <TodosContext.Provider value={{ state, dispatch }}>
+            <TodosList/>
+        </TodosContext.Provider>
+    )
+}
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
